01ee9e92dd8a80aa5c8a4519605924852230c24b
Abdulkadir Arslan <47847116+WazaAbdulkadir@users.noreply.github.com>
2023-01-19 10:35:52 +0300

    bitstream rad

--- image_decoder.py
+++ image_decoder.py
@@ -1,3 +1,15 @@
-x = [1,3] + [2,5]
 
-print(x) 
+import numpy as np
+
+#arr = np.array([1, 2, 3, 4, 5])
+#print(arr) 
+
+file = open("example.bin","rb")
+byte_read = file.read()
+file.close()
+
+print(f'bytes: {len(byte_read)}') 
+
+
+
+#print (2+3)
